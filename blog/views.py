8e3c722bbf242bf2f8a7bbcd18a516beb3cf086b
kassymoto <vocalo.angel.ag@gmail.com>
2018-07-28 18:51:50 +0900

    second

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,12 @@ from .forms import MyForm
 from django.http import QueryDict
 import os
 import csv
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+
 
 
 def kakeibo(request):
@@ -38,23 +41,20 @@ def kakeibo(request):
 def plot_graph(cs):
     data=cs.values()
     label=cs.keys()
-    os.remove('blog/static/images/simple.png')
-    ###綺麗に書くためのおまじない###
+
     plt.style.use('ggplot')
     plt.rcParams.update({'font.size':15})
 
-    ###各種パラメータ###
-    size=(9,5) #凡例を配置する関係でsizeは横長にしておきます。
-    col=cm.Spectral(np.arange(len(data))/float(len(data))) #color指定はcolormapから好みのものを。
+    size=(9,5)
+    col=cm.Spectral(np.arange(len(data))/float(len(data)))
 
-    ###pie###
     plt.figure(figsize=size,dpi=100)
     plt.pie(data,colors=col,counterclock=False,startangle=90,autopct=lambda p:'{:.1f}%'.format(p) if p>=5 else '')
     plt.subplots_adjust(left=0,right=0.7)
     plt.legend(label,fancybox=True,loc='center left',bbox_to_anchor=(0.9,0.5))
     plt.axis('equal')
     plt.savefig('blog/static/images/simple.png',bbox_inches='tight',pad_inches=0.05)
-    plt.close()
+    plt.close("all")
 
 def save_csv(category, price):
     #CSVを開く
